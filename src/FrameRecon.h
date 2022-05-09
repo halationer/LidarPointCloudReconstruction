ea5be298b80b91a85994192e16c45dbe708cbe5c
ludy <alualu628628@163.com>
2022-05-09 01:22:23 -0400

    revert

--- src/FrameRecon.h
+++ src/FrameRecon.h
@@ -1,6 +1,6 @@
-
 #ifndef FRAMERECON_H
 #define FRAMERECON_H
+
 #include <string>
 #include <ctime>
 
@@ -13,19 +13,32 @@
 #include "pcl_ros/transforms.h"  
 #include <pcl_conversions/pcl_conversions.h>
 
+//polygon related
+#include <geometry_msgs/PolygonStamped.h>
+#include <geometry_msgs/Point32.h>
+
 //project related
 #include "GHPR.h"
 #include "SectorPartition.h"
 #include "ExplicitRec.h"
+#include "CircularVector.h"
 
-//octomap related
-//#include <octomap/octomap.h>
-//#include <octomap_msgs/Octomap.h>
-//#include <octomap_msgs/GetOctomap.h>
-//#include <octomap_msgs/conversions.h>
+// Trajectory state data. 
+struct RosTimePoint{
 
-//#include <grid_map_octomap/GridMapOctomapConverter.hpp>//transfor octomap to grid
+	// The time of the measurement leading to this state (in seconds). //
+	ros::Time oTimeStamp;
 
+	//********angles***********
+	//float roll;    ///< The current roll angle. 
+	//float pitch;    ///< The current pitch angle.
+	//float yaw;   ///< The current yaw angle. 
+
+	//********coordinate value****************
+	// The global trajectory position in 3D space. 
+	pcl::PointXYZ oLocation;
+
+}
 
 //******************************************************************
 // this class below is to compute topological guidance map based on SLAM
@@ -56,43 +69,9 @@ class FrameRecon{
   virtual ~FrameRecon();
 
   //Reads and verifies the ROS parameters.
-  bool ReadLaunchParams(ros::NodeHandle & nodeHandle);
+  bool ReadLaunchParams(ros::NodeHandle & nodeHandle);  
 
-
-  //down sample the point clouds with grid idxs
-  void SamplingPointClouds(pcl::PointCloud<pcl::PointXYZ>::Ptr & pCloud,
-                          std::vector<std::vector<int> > & vPointMapIdx,
-                                                       int iSmplNum = 3);
-
-  //down sampling the point clouds with grid idxs and corresponding labels
-  void SamplingPointClouds(pcl::PointCloud<pcl::PointXYZ>::Ptr & pCloud,
-                          std::vector<std::vector<int> > & vPointMapIdx,
-                                        std::vector<int> & vCloudLabels,
-                                                       int iSmplNum = 3);
-
-  
-  //*************Traversing / retrieving function*************
-
-
-  //extract the point clouds from the given neighboring grids
-  void ExtractLabeledPCs(pcl::PointCloud<pcl::PointXYZ> & vNearGrndClouds,
-                                   std::vector<int> & vNearGroundGridIdxs,
-                        pcl::PointCloud<pcl::PointXYZ> & vNearBndryClouds,
-                         pcl::PointCloud<pcl::PointXYZ> & vNearObstClouds,                                           
-                                const std::vector<MapIndex> & vNearByIdxs,
-                                                    const int & iNodeTime);
-
-  void ExtractLabeledPCs(pcl::PointCloud<pcl::PointXYZ> & vNearGrndClouds,
-                        pcl::PointCloud<pcl::PointXYZ> & vNearBndryClouds,
-                          pcl::PointCloud<pcl::PointXYZ> & vNearAllClouds,
-                                   std::vector<int> & vNearGroundGridIdxs,
-                                const std::vector<MapIndex> & vNearByIdxs);
-
-  void ExtractLabeledPCs(pcl::PointCloud<pcl::PointXYZ> & vNearGrndClouds,
-                        pcl::PointCloud<pcl::PointXYZ> & vNearBndryClouds,
-                                   std::vector<int> & vNearGroundGridIdxs,
-                                const std::vector<MapIndex> & vNearByIdxs);
-
+  void SamplePoints(const pcl::PointCloud<pcl::PointXYZ> & vCloud, pcl::PointCloud<pcl::PointXYZ> & vNewCloud, int iSampleNum, bool bIntervalSamp);
 
   //*************handler function*************
   //handle the trajectory information
@@ -100,16 +79,8 @@ class FrameRecon{
   void HandleTrajectory(const nav_msgs::Odometry & oTrajectory);
 
   //handle the ground point clouds topic
-  void HandleGroundClouds(const sensor_msgs::PointCloud2 & vGroundRosData);
+  void HandlePointClouds(const sensor_msgs::PointCloud2 & vCloudRosData);
 
-  //handle the boundary point cloud topic
-  void HandleBoundClouds(const sensor_msgs::PointCloud2 & vBoundRosData);
-
-  //handle the obstacle point cloud topic
-  void HandleObstacleClouds(const sensor_msgs::PointCloud2 & vObstacleRosData);
-
-  //update octomap octree nodes
-  //void UpdatingOctomapNodes();  
   //*************Output function*************
   //publish point clouds
   void PublishPointCloud(pcl::PointCloud<pcl::PointXYZ> & vCloud);
@@ -117,150 +88,51 @@ class FrameRecon{
 
  private:
 
-  //**input topic related**
-  //output file name
-  std::string m_sFileHead;
+  //***for input odom topic***
+  //the m_oOdomSuber subscirber is to hearinput  odometry topic
+  ros::Subscriber m_oOdomSuber;
 
-  std::stringstream m_sCoverFileName; ///<full name of output txt that records the point clouds//defind it in the function
-  bool m_bCoverFileFlag; //whether the coverage file got a full name or not 
-  std::ofstream m_oCoverFile;
+  //the name of input odometry topic (robot trajectory)
+  std::string m_sOdomTopic;
 
-  std::stringstream m_sMapFileName; ///<full name of output txt that records the point clouds//defind it in the function
-  bool m_bMapFileFlag; //whether the coverage file got a full name or not 
-  std::ofstream m_oMapFile;
+  unsigned int m_iTrajFrameNum;
 
-  std::stringstream m_sOutTrajFileName;///<full name of output txt that records the trajectory point 
-  bool m_bOutTrajFileFlag;//whether the trajectory file got a full name or not 
-  std::ofstream m_oTrajFile;
+  //***for input point cloud topic***
+  //the m_oCloudSuber subscirber is to hear input point cloud topic
+  ros::Subscriber m_oCloudSuber;
 
-  std::stringstream m_sOutPCFileName;///<full name of output txt that records the scanning point clouds 
-  bool m_bOutPCFileFlag;//whether the point cloud recording file got a full name or not 
-  std::ofstream m_oPCFile;
+  //the name of input point cloud topic 
+  std::string m_sCloudTopic; 
 
-  std::stringstream m_sOutNodeFileName;///<full name of output txt that records the visited node position 
-  bool m_bOutNodeFileFlag;//whether the point cloud recording file got a full name or not 
-  std::ofstream m_oNodeFile;
 
-  //input topics:
-  ros::Subscriber m_oOdomSuber;//the subscirber is to hear (record) odometry from gazebo
+  //How many frames of point cloud have been calculated cumulatively
+  unsigned int m_iPCFrameCount;
 
-  ros::Subscriber m_oGroundSuber;//the subscirber is to hear (record) odometry from gazebo
-  ros::Subscriber m_oBoundSuber;//the subscirber is to hear (record) odometry from gazebo
-  ros::Subscriber m_oObstacleSuber;//the subscirber is to hear (record) odometry from gazebo
+  //frame sampling
+  unsigned int m_iFrameSmpNum;
 
-  std::string m_sOdomTopic;  //the topic name of targeted odometry (robot trajectory)
-
-  std::string m_sGroundTopic; //the topic name of targeted odometry (robot trajectory)
-
-  std::string m_sBoundTopic; //the topic name of targeted odometry (robot trajectory)
-
-  std::string m_sObstacleTopic; //the topic name of targeted odometry (robot trajectory)
-
-  //**output topics related**
-
-  ros::Publisher m_oGridMapPublisher;//! Grid map publisher.
-
-  //ros::Publisher m_oOctomapPublisher; //! Octomap publisher.
-  //publishing point cloud is only for test
-  ros::Publisher m_oCloudPublisher;// point cloud publisher for test
-
-  ros::Publisher m_oGoalPublisher;// goal odometry information publisher
-
-  ros::Publisher m_oPlanNodePublisher;// planning nodes publisher for display
-
-  ros::Publisher m_oPastNodePublisher;// past nodes publisher for display
+  //sampling number of input point clouds
+  unsigned int m_iSampleInPNum;
+  
+  //***for output point cloud topic***
+  //point cloud publisher for test
+  ros::Publisher m_oCloudPublisher;
 
   //**frenquency related**
-  
-  double m_dOdomRawHz;//the raw frequency of odometry topic (50hz, or 10hz in normal)
-  
-  double m_dSamplingHz;//the raw frequency of odometry topic (50hz, or 10hz in normal)
 
   float m_fViewZOffset;//z offset of odom to lidar sensor
   
-  //sampling number
-
-  int m_iOdomSampingNum;   //odometry sampling number is equal to int(m_dOdomRawHz / m_dSamplingHz);
-
-  int m_iPCSmplNum; //point clouds sampling number for all classification point clouds
-  
-  //int m_iMapFreshNum; //received octomap updated time interval is equal to int(m_dOdomRawHz / m_dOctomapFreshHz);
-
-  int m_iComputedFrame;//the times of actual calculations
-
-  int m_iPastOdomNum; //the past view interval number for occlusion calculation
-
-  int m_iShockNum;//the shock duration that robot can tolerate 
-
-  int m_iRecordPCNum;//the times of recording point cloud frame (any category) in output file
-
   //the frame count of trajectory point
-  unsigned int m_iTrajFrameNum;
 
-  unsigned int m_iGroundFrames;
 
-  unsigned int m_iBoundFrames;
+  //explicit reconstruction
+  ExplicitRec m_oExplicitBuilder;
 
-  unsigned int m_iObstacleFrames;
+  //circle vector of odom
+  CircularVector<RosTimePoint> m_vOdomHistory;
 
-  unsigned int m_iNodeTimes;//the times of node to be visited
 
-  //**point cloud related**
-  //the positions of robot
-  std::queue<pcl::PointXYZ> m_vOdomViews;//I dont think it is necessary to use a circle vector
-  std::queue<pcl::PointXYZ> m_vOdomShocks;
-  
-  pcl::PointCloud<pcl::PointXYZ>::Ptr m_pBoundCloud;//boundary point clouds
-  pcl::PointCloud<pcl::PointXYZ>::Ptr m_pObstacleCloud;//obstacle point clouds
-  std::vector<int> m_vObstNodeTimes;//records the acquired times (node times) of each obstacle point
 
-  //std::vector<std::vector<int> > m_vGroundPntMapIdx;//ground point index in grid map
-  std::vector<std::vector<int> > m_vBoundPntMapIdx;//boundary point index in grid map
-  std::vector<std::vector<int> > m_vObstlPntMapIdx;//obstacle point index in grid map
-
-  //the map - main body 
-  ExtendedGM m_oGMer;
-
-  Confidence m_oCnfdnSolver;//confidence object
-
-  std::vector<ConfidenceValue> m_vConfidenceMap;//Confidence value map
-
-  //the grid map initialization flag indicates whether the map has been simply established
-  bool m_bGridMapReadyFlag;
-
-  //a node object
-  OP m_oOPSolver;
-
-  //a Astar estimator
-  Astar m_oAstar;
-
-  //a local path optimer
-  PathOptimization oLclPthOptimer;
-
-  //some paramters which is no more important
-  //the radius which initial the travelable region around robot at original location
-  float m_fInitialR;
-
-  //follow anchor path or not
-  //flag indicate the path selection
-  bool m_bAnchorGoalFlag; 
-  
-  //goal point of global path planning
-  pcl::PointXYZ m_oNodeGoal;
-
-  //anchor goals - local path
-  pcl::PointCloud<pcl::PointXYZ> m_vAncherGoals;
-
-  //count visited anchor in a trip
-  int m_iAncherCount;
-
-  //time recorded
-  double oDisTermDur;
-  double oBoundTermDur;
-  double oVisTermDur;
-  double oNodeDur;
-  double oFractTermDur;
-  double oLocalPathTermDur;
   
 };
 
